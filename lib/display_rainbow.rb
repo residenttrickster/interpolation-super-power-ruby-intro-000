6f78454211e5b2340a32e759eeb8661be8905121
Anastasia Orlova <metkusblackus@gmail.com>
2020-02-14 06:43:26 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
  def display_rainbow (color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
 puts  "R: #{color [0]}, O: #{color [1]}, Y: #{color [2]}, G: #{color [3]}, B: #{color [4]}, I: #{color[5]}, V: color #{[6]}"
  end
-display_rainbow
+display_rainbow (color)
